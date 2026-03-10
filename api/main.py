e8b9c23d77a7b18d1bfcd37125f1bfa80233d7f0
Daniel Varela <ubuntu@ip-172-31-89-176.ec2.internal>
2026-03-10 16:23:13 +0000

    Correciond de errores

--- api/main.py
+++ api/main.py
@@ -14,7 +14,7 @@ S3_BUCKET_NAME = os.getenv("S3_BUCKET_NAME")
 
 # Ajusta esto al nombre real de tu archivo .pkl guardado en la Fase C
 MODEL_FILE_NAME = "model.pkl" 
-S3_MODEL_CARD = "data/produccion/Modelos/"
+S3_MODEL_CARD = "data/produccion/Modelos"
 S3_MODEL_PREFIX = "data/produccion/Modelos/tmp_champion_model/"
 
 # ==========================================
